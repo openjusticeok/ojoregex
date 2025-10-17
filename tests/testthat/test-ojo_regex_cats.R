d2edf67a03252f4f27a5cac3a4b33b2e9e1a3f53
Brancen Gregory <brancengregory@gmail.com>
2025-10-17 02:01:48 -0500

    Update flags and cats snapshots to be file type snaps

--- tests/testthat/test-ojo_regex_cats.R
+++ tests/testthat/test-ojo_regex_cats.R
@@ -1,3 +1,7 @@
 test_that("regex cats haven't changed", {
-  expect_snapshot(ojoregex::ojo_regex_cats)
+  temp_path <- tempfile(fileext = "csv")
+
+  readr::write_csv(ojo_regex_cats, temp_path)
+  
+  expect_snapshot_file(temp_path, "ojo_regex_cats.csv")
 })
--- tests/testthat/test-ojo_regex_flags.R
+++ tests/testthat/test-ojo_regex_flags.R
@@ -1,3 +1,7 @@
 test_that("regex flags haven't changed", {
-  expect_snapshot(ojoregex::ojo_regex_flags)
+  temp_path <- tempfile(fileext = "csv")
+
+  readr::write_csv(ojo_regex_flags, temp_path)
+  
+  expect_snapshot_file(temp_path, "ojo_regex_flags.csv")
 })
