fc1156655e64fbfa413af077fc3c7fa79a5943e2
Brancen Gregory <brancengregory@gmail.com>
2025-10-24 13:02:59 -0500

    Format

--- tests/testthat/test-pre_clean.R
+++ tests/testthat/test-pre_clean.R
@@ -4,13 +4,13 @@ test_that("Pre-cleaning works the same", {
   load(test_path("test_data.rda"))
 
   clean_test_data <- test_data |>
-      dplyr::mutate(
-        clean_description = regex_pre_clean(description)
-      )
+    dplyr::mutate(
+      clean_description = regex_pre_clean(description)
+    )
 
   temp_path <- tempfile(fileext = "csv")
 
   readr::write_csv(clean_test_data, temp_path)
-  
+
   expect_snapshot_file(temp_path, "clean_test_data.csv")
 })
